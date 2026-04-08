8facc316c8d1807e3918ebd5cb6dc09fbcaf6ac8
Prem Karhale <premkarhale383@gmail.com>
2026-04-08 13:16:14 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://13.201.70.18:8000',
+        changeOrigin: true
+      }
+    }
+  }
 })
